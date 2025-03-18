5ed0c0fe953494c3ffb27e1c9affc5e4f3cc2b80
buildxhq <anthony@buildxhq.com>
2025-03-18 13:58:25 -0500

    Update files.js

--- routes/files.js
+++ routes/files.js
@@ -4,13 +4,9 @@ const { uploadFile } = require('../controllers/fileController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
-
-// Multer Storage Configuration (For Temporary Uploads Before S3)
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Upload File Route (Protected)
 router.post('/upload', authMiddleware, upload.single('file'), uploadFile);
 
 module.exports = router;
-
