b9d5f25e4a7f867f46f92beb2287620d155a2a55
buildxhq <anthony@buildxhq.com>
2025-03-18 13:38:28 -0500

    Update projects.js
    Project Routes

--- routes/projects.js
+++ routes/projects.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const { createProject, getAllProjects } = require('../controllers/projectController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post('/', authMiddleware, createProject);
+router.get('/', getAllProjects);
+
+module.exports = router;
 
