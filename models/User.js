7f11223e65bc4ffd91af789cc1b46da1b1a08d6b
buildxhq <anthony@buildxhq.com>
2025-03-18 13:34:53 -0500

    Update User.js
    User Model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
+const knex = require('../config/db');
+
+const createUser = async (email, password) => {
+    return knex('users').insert({ email, password }).returning('*');
+};
+
+const findUserByEmail = async (email) => {
+    return knex('users').where({ email }).first();
+};
+
+module.exports = { createUser, findUserByEmail };
 
