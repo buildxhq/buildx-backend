22043fad11484986e86e421c46b40439db6ed838
buildxhq <anthony@buildxhq.com>
2025-03-18 13:35:25 -0500

    Update Project.js
    Project Model

--- models/Project.js
+++ models/Project.js
@@ -1,1 +1,12 @@
+const knex = require('../config/db');
+
+const createProject = async (name, description, ownerId) => {
+    return knex('projects').insert({ name, description, owner_id: ownerId }).returning('*');
+};
+
+const getAllProjects = async () => {
+    return knex('projects').select('*');
+};
+
+module.exports = { createProject, getAllProjects };
 
