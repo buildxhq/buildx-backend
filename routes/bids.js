6d5831eb26c82f31488f7f3c557251f27eec3d72
buildxhq <anthony@buildxhq.com>
2025-03-18 13:39:01 -0500

    Update bids.js
    Bidding Routes

--- routes/bids.js
+++ routes/bids.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const { createBid, getBidsForProject } = require('../controllers/bidController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post('/', authMiddleware, createBid);
+router.get('/:projectId', getBidsForProject);
+
+module.exports = router;
 
