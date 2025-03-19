e899871e1ea965da042978a2928ae13e91433346
Anthony Anderson <anthony@buildxhq.com>
2025-03-18 23:21:54 -0500

    Fixed authentication routes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 
+const authRoutes = require('./routes/auth'); // Make sure this exists
+const projectRoutes = require('./routes/projects');
+const bidRoutes = require('./routes/bids');
+
 const app = express();
 app.use(express.json());
+
+// Load API routes
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/bids', bidRoutes);
 
 const PORT = process.env.PORT || 5000;
 
