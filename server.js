755a096973d3a0ae6a27e5efc5f6f988b31b9b8c
Anthony Anderson <anthony@buildxhq.com>
2025-03-18 23:16:44 -0500

    Added root route to server.js

--- server.js
+++ server.js
@@ -6,5 +6,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.json({ message: "🔥 BuildX Backend is Live & API is Working! 🔥" });
+});
+
+
 app.listen(PORT, '0.0.0.0', () => console.log(`🔥 Server Running on Port ${PORT}`));
 
